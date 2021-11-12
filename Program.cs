065b46ef909c08a1741132a983f7850768079d76
Marat <farkhutdinovmr@gmail.com>
2021-11-12 16:32:00 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pictures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pictureCount = 52;
+            int pictureCountInRow = 3;
+
+            Console.WriteLine("Количество заполненных рядов: " + pictureCount / pictureCountInRow);
+            Console.WriteLine("Количество оставшихся картинок: " + pictureCount % pictureCountInRow);
+        }
+    }
+}
